d25e4fc3e9fc6ab03f9c97be01157c968f76cb70
rogers <rogers@rogerogers.com>
2022-07-05 00:05:30 +0800

    test: simple

--- config_test.go
+++ config_test.go
@@ -1,11 +1,49 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/polarismesh/polaris-go"
 )
+
+func makeJsonRequest(uri string, data string, method string) ([]byte, error) {
+	client := http.Client{}
+	req, err := http.NewRequest(method, uri, strings.NewReader(data))
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		return nil, err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
+}
+
+func TestJsonPost(t *testing.T) {
+	data, err := json.Marshal(map[string]string{
+		"name":      "test.yaml",
+		"namespace": "default",
+		"group":     "test",
+		"content":   "useLocalCache: true",
+		"modifyBy":  "polaris",
+		"format":    "yaml",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := makeJsonRequest("http://localhost:8093/config/v1/configfiles", string(data), http.MethodPost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", res)
+}
 
 func TestConfig(t *testing.T) {
 	configApi, err := polaris.NewConfigAPI()
@@ -23,7 +61,7 @@ func TestConfig(t *testing.T) {
 	for k, v := range kv {
 		fmt.Printf("%d-%s", k, v)
 	}
-	w, err := config.Watch()
+	w, _ := config.Watch()
 	defer func() {
 		w.Stop()
 	}()
@@ -32,10 +70,13 @@ func TestConfig(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	newConfig, err := configApi.GetConfigFile("default", "aaa", "a.yaml")
+	// newConfig, err := configApi.GetConfigFile("default", "aaa", "a.yaml")
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// makeJsonPost("http://localhost:8093/config/v1/configfiles")
+	// http.NewRequest(http.MethodPost, "localhost:8083/")
 
 	fmt.Println(v)
 }
